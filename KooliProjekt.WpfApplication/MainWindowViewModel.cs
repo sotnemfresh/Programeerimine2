3b5b434a1c05577ae2bf91049a54aa94ff00f06a
sotnemfresh <98352598+sotnemfresh@users.noreply.github.com>
2026-06-30 21:50:12 +0300

    09.04 Tund ÜL: WPF-rakenduse ehitamine.
    Lisatud Visual Studio solutionisse WPF Application.
    WPF rakendus käivitub ning DataGridis mööda ridu liikudes muutuvad andmed paremal pool tekstikastides.

---
+++ KooliProjekt.WpfApplication/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KooliProjekt.WpfApplication
+{
+    public class MainWindowViewModel
+    {
+        public IList<Toode> Data 
+        { 
+            get
+            {
+                var items = new List<Toode>
+                {
+                    new Toode { Id = 1, Name = "Test 1" },
+                    new Toode { Id = 2, Name = "Test 2" },
+                    new Toode{ Id = 3, Name = "Test 3" }
+                };
+
+                return items;
+            }
+        }
+
+        public object SelectedItem { get; set; }
+    }
+}
